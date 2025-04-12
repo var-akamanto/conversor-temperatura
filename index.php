df3e5ca3b124a2b5df6dbe96e4aa36905cce9afb
Rafael Antunes <rafinha72465@gmail.com>
2025-04-11 21:04:56 -0300

    começo php

--- index.php
+++ index.php
@@ -52,15 +52,55 @@
     }
   </style>
 </head>
+
+
+
 <body>
+
+  <!-- Parte PHP -->
+   <?php 
+    $celsius;
+    $fahreinhent;
+    $kelvin;
+
+    //função celsius -> fahrenheit
+   function conversor1 ($celsius){
+    $temperatura;
+    return ($temperatura * 9/5) + 32;
+   }
+
+    //função celsius -> Kelvin
+    function conversor 2 ($celsius){
+      $temperatura;
+      return $temperatura + 273.15;
+    }
+
+    //fahrenheit -> kelvin = converte para celsius e depois para kelvin. (o contrário se aplica)
+
+
+    // Função de verificar (ao clicar em qualquer um dos botões da esquerda ) 
+      //primeiro teste: se o campo input da esquerda está vazio ou não.
+      // Ler qual é o tipo de temperatura informada:
+        // if botão 1-esquerda estiver ativo: valor que está dentro do input da esquerda é em celsius
+        // if botão 2-esquerda estiver ativo: valor que está dentro do input da esquerda é em fahrenheit
+        // if botão 3-esquerda estiver ativo: valor que está dentro do input da esquerda é em kelvin
+      // reliza a função de conversão (uma função que recebe qual o tipo de conversão  e qual a temperatura)
+
+   ?>
+  
+
+  <!-- Funções -->  
+
+
+
   <div class="container">
     <!-- Bloco da Esquerda -->
     <div class="bloco">
       <input type="text" class="input-temperatura" placeholder="Temperatura">
       <div class="botoes">
-        <button class="botao"></button>
-        <button class="botao"></button>
-        <button class="botao"></button>
+        <button class="botao" id="1-esquerda">Celsius</button>
+        <button class="botao" id="2-esquerda">Fahrenheit</button>
+        <button class="botao" id="3-esquerda">Kelvin</button>
       </div>
     </div>
 
